8a58149686f7adfc8bab3a9ed8fda97eb02bb7a1
camedool <2long4live@gmail.com>
2019-06-16 22:14:01 +0200

    Changing the loaiding placeholder locaiton

--- js/popup.js
+++ js/popup.js
@@ -9,39 +9,34 @@ function init() {
         let loaderPlaceholder = document.getElementById("loader_placeholder");
         loaderPlaceholder.classList.add("loading");
         button.style.visibility = "hidden";
+        button.style.position = "absolute";
 
-        // @ts-ignore
         let toLanguage;
         // @ts-ignore
-        if (document.getElementById("latin").checked) {
+        if (document.getElementById("latin").checked) 
             toLanguage = "latin";
-            console.log("latins is being transliterated");
-        } else {
+        else 
             toLanguage = "cyrillic"
-            console.log("cyrillic is being transliterated");
-        }
 
         const params = {
             active: true,
             currentWindow: true
         }
 
-        // for(let i=0; i<50000; i++) {
-        //     console.log(i);
-        // }
-
-        // @ts-ignore
-        chrome.tabs.query(params, (tabs) => {
+        setTimeout(() => {
             // @ts-ignore
-            chrome.tabs.sendMessage(tabs[0].id, {language: toLanguage}, (response) => {
-                // if (response.success) {
-                //     loaderPlaceholder.classList.remove("loading");
-                //     // remove the button class
-                //     button.style.visibility = "visible";
-
-                // }
-            })
-        });
+            chrome.tabs.query(params, (tabs) => {
+                // @ts-ignore
+                chrome.tabs.sendMessage(tabs[0].id, {language: toLanguage}, (response) => {
+                    if (response.success) {
+                        loaderPlaceholder.classList.remove("loading");
+                        // remove the button class
+                        button.style.visibility = "visible";
+                        button.style.position = "relative"
+                    }
+                })
+            });
+        }, 0)
     });
 }
 
